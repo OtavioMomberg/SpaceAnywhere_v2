25a3cdd4ac3012b183c8edfaa8a3dc99c3d29191
OtavioMomberg <otavioaugusto777444@gmail.com>
2026-09-23 12:17:19 -0300

    chore: path_provider package removed

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -8,36 +8,41 @@ import 'package:space_anywhere/features/wallpaper/screens/wallpaper_screen.dart'
 
 enum TransitionType { slideLtoR, slideRtoL, fade, scale }
 
-final map = {
-  TransitionType.slideLtoR: ({required Animation<double> animation, required Widget child}) {
-    const begin = Offset(-1.0, 0.0);
-    const end = Offset.zero;
-    const curve = Curves.easeInOut;
-    final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+Widget getTranslation({
+  required Animation<double> animation, 
+  required Widget child,
+  required TransitionType type
+}) {
+  switch (type) {
+    case .slideLtoR:
+      const begin = Offset(-1.0, 0.0);
+      const end = Offset.zero;
+      const curve = Curves.easeInOut;
+      final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
-    return SlideTransition(position: animation.drive(tween), child: child);
-  },
-  TransitionType.slideRtoL: ({required Animation<double> animation, required Widget child}) {
-    const begin = Offset(1.0, 0.0);
-    const end = Offset.zero;
-    const curve = Curves.easeInOut;
-    final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      return SlideTransition(position: animation.drive(tween), child: child);
 
-    return SlideTransition(position: animation.drive(tween), child: child);
-  },
-  TransitionType.fade:({required Animation<double> animation, required Widget child}) {
-    return FadeTransition(
-      opacity: CurvedAnimation(parent: animation, curve: Curves.easeInOut),
-      child: child,
-    );
-  },
-  TransitionType.scale: ({required Animation<double> animation, required Widget child}) {
-    return ScaleTransition(
-      scale: CurvedAnimation(parent: animation, curve: Curves.easeInOut),
-      child: child,
-    );
+    case .slideRtoL:
+      const begin = Offset(1.0, 0.0);
+      const end = Offset.zero;
+      const curve = Curves.easeInOut;
+      final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(position: animation.drive(tween), child: child);
+
+    case .fade:
+      return FadeTransition(
+        opacity: CurvedAnimation(parent: animation, curve: Curves.easeInOut),
+        child: child,
+      );
+      
+    case .scale:
+      return ScaleTransition(
+        scale: CurvedAnimation(parent: animation, curve: Curves.easeInOut),
+        child: child,
+      );
   }
-};
+}
 
 class AppRoutes {
   static const pages = [
@@ -67,14 +72,10 @@ class AppRoutes {
       transitionDuration: const Duration(milliseconds: 350),
       reverseTransitionDuration: const Duration(milliseconds: 400),
       transitionsBuilder: (_, animation, _, child) {
-        final transition = map[type];
-
-        if (transition != null) {
-          return transition(animation: animation, child: child);
-        }
-        return map[TransitionType.fade]!(
-          animation: animation,
-          child: child,
+        return getTranslation(
+          animation: animation, 
+          child: child, 
+          type: type
         );
       },
     );
